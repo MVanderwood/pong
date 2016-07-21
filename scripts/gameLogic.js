b07b126b5936cec867f779eca6d7c5e7e402ed73
MVanderwood <m.b.vanderwood@gmail.com>
2016-07-21 18:58:26 -0500

    Adds basic logic questions.

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -1,3 +1,17 @@
 (function() {
-    
+    var LogicEngine = {
+        movePaddle: function(paddleY, direction) {
+            if (direction === 'up') { paddleY--; }
+            else if (direction === 'down') { paddleY++; }
+        },
+        moveBall: function(ballX, ballY, yDirection, xDirection) {
+            if (yDirection === 'up') { ballY--; }
+            else if (yDirection === 'down') { ballY++; }
+
+            if (xDirection === 'left') { ballX--; }
+            else if (xDirection === 'right') { ballX++; }
+        }
+    };
+
+    window.LogicEngine = LogicEngine;
 })();
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
-(function() {
+(function(renderer) {
+    var ballX = 300;
+    var ballY = 200;
+    var paddleOneX = 10;
+    var paddleOneY = 150;
+    var paddleTwoX = 590;
+    var paddleTwoY = 150;
 
-})();
+    function gameLoop() {
+        renderer.clearCanvas();
+        renderer.drawBall(ballX, ballY);
+        renderer.drawPaddle(paddleOneX, paddleOneY);
+        renderer.drawPaddle(paddleTwoX, paddleTwoY);
+    }
+
+    var fps = 60;
+    setInterval(gameLoop, 1000 / fps);
+})(window.Renderer);
